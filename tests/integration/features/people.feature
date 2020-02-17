bcfaa0deb10a9ca5418566acaa464ad7248df4e9
Alisson Claudino <alisson.claudino@azion.com>
2020-02-17 02:48:31 -0300

    feat: add integration test

---
+++ tests/integration/features/people.feature
@@ -0,0 +1,15 @@
+Feature: People register
+
+    I want to register my infos in the people register
+
+    Scenario Outline: Adding a person to the register
+        Given I've accessed the people register view
+        When I fill the fields with name "<name>", age "<age>", email "<email>"
+        And I click in the submit button
+        Then a card with name "<name>", age "<age>", email "<email>" should appear
+
+        Examples:
+        | name           |  age  |  email             |
+        | xuxa meneghel  |  12   | xuxa@meneghell.com |
+        | sasha meneghel |  19   | sasha@park.com     |
+
---
+++ tests/integration/steps/people.js
@@ -0,0 +1,28 @@
+import { Given, When, Then } from 'cucumber';
+import { Selector } from 'testcafe';
+
+Given('I\'ve accessed the people register view', async (t) => {
+    await t.navigateTo('http://localhost:8080/');
+});
+
+When(/^I fill the fields with name "(.+)", age "(.+)", email "(.+)"$/, async (t, [name, age, email]) => {
+    await t.typeText('.form-card .field-name input', name);
+    await t.typeText('.form-card .field-age input', age);
+    await t.typeText('.form-card .field-email input', email);
+});
+
+When('I click in the submit button', async (t) => {
+    await t.click('.form-card button');
+});
+
+Then(/^a card with name "(.+)", age "(.+)", email "(.+)" should appear$/, async (t, [name, age, email]) => {
+    await t.expect(
+        Selector('.person-card .v-card__title').textContent,
+    ).contains(name);
+    await t.expect(
+        Selector('.person-card .age').textContent,
+    ).contains(age);
+    await t.expect(
+        Selector('.person-card .email').textContent,
+    ).contains(email);
+});
